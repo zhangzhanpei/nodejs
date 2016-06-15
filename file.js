2fac2ed16ce4e2418383fcabdcfcf1a4576bee0f
zhanpei <zhangzhanpei@gmail.com>
2016-06-15 22:45:38 +0800

    读取文件
    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

--- file.js
+++ file.js
@@ -14,3 +14,13 @@ fs.writeFile('message.txt', 'Hello, Node.js!\n', (err) => {
 
 //同步写文件
 fs.writeFileSync('message.txt', '同步写文件!\n');
+
+//异步读文件
+fs.readFile('message.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
+//同步读文件
+let fileContent = fs.readFileSync('message.txt', 'utf8');
+console.log(fileContent);

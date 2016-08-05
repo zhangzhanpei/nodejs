b44fdd06904dc2717f23ab4981be8814cc67db45
zhanpei <zhangzhanpei@gmail.com>
2016-08-05 21:15:49 +0800

    js数据类型
    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

---
+++ dataTypes.js
@@ -0,0 +1,21 @@
+//undefined类型
+let x;
+
+//number类型，js只有一种number类型，所以6.0也是number类型，而不是float
+let y1 = 6;
+let y2 = 12.5;
+let y3 = 1e5; //100000
+let y4 = 123e-5; //0.00123
+
+//string类型
+let s1 = "My name is 'Xiaofan'";
+
+//boolean类型
+let t = true;
+let f = false;
+
+//array类型, 中括号
+let fruit = ["apple", "banana", "orange"];
+
+//object类型, 花括号
+let human = {name:"Xiaofan", age:25, gender:'male'}

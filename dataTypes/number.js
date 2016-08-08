babd275c8e2a8ce7768eafbc4b4543c2450533b7
zhanpei <zhangzhanpei@gmail.com>
2016-08-08 21:31:15 +0800

    number/string
    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

---
+++ dataTypes/number.js
@@ -0,0 +1,5 @@
+let n = 13.37;
+let k = 10000;
+console.log(n.toFixed(1)); //4舍5入, 保留一位小数
+console.log(n.toString(2)); //转为2进制的字符串，如不填则以10为基数
+console.log(k.toPrecision(3)); //转为指数计数法，显示3位
---
+++ dataTypes/string.js
@@ -0,0 +1,4 @@
+let str = 'Hello, Js!';
+console.log(str.length); //获取字符串长度
+console.log(str.toLowerCase()); //字符串小写
+console.log(str.charAt(1)); //获取相应位置的字符

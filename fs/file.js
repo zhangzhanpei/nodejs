9f0b6d0a0eeab6eb34e7a20bc08443fbdadfebdd
zhanpei <zhangzhanpei@gmail.com>
2017-04-29 19:31:10 +0800

    Signed-off-by: zhanpei <zhangzhanpei@gmail.com>

--- fs/file.js
+++ fs/file.js
@@ -1,26 +1,26 @@
 const fs = require('fs')
 
 //追加写文件
-fs.appendFile('message.txt', '追加写文件\n', (err) => {
+fs.appendFile('./message.txt', '追加写文件\n', (err) => {
     if (err) throw err;
     console.log('追加写文件成功');
 });
 
 //异步写文件
-fs.writeFile('message.txt', 'Hello, Node.js!\n', (err) => {
+fs.writeFile('./message.txt', 'Hello, Node.js!\n', (err) => {
     if (err) throw err;
     console.log('异步写文件成功');
 });
 
 //同步写文件
-fs.writeFileSync('message.txt', '同步写文件!\n');
+fs.writeFileSync('./message.txt', '同步写文件!\n');
 
 //异步读文件
-fs.readFile('message.txt', 'utf8', (err, data) => {
+fs.readFile('./message.txt', 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
 });
 
 //同步读文件
-let fileContent = fs.readFileSync('message.txt', 'utf8');
+let fileContent = fs.readFileSync('./message.txt', 'utf8');
 console.log(fileContent);
--- module/index.js
+++ module/index.js
@@ -1,2 +1,2 @@
-const circle = require('./module/circle.js');
+const circle = require('./circle.js');
 console.log(`半径为3的圆面积为${circle.area(3)}`);
